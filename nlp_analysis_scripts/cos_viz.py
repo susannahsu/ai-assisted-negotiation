4e98d5bfb0b7f7ea505c003269ed9513b93f2136
Susannah Su <susannahsu1201@gmail.com>
2024-03-30 20:37:23 -0400

    Finish generating heatmaps for hfa case (both BERT and non-BERT, and iceberg css and IoA); will need to adjust the temperature metor though

--- nlp_analysis_scripts/cos_viz.py
+++ nlp_analysis_scripts/cos_viz.py
@@ -4,11 +4,10 @@ import matplotlib.pyplot as plt
 
 # Loading the similarity matrices
 # cosine_sim_matrix = np.load('bert_cosine_similarity_matrix.npy')
-cosine_sim_matrix = np.load('bert_cosine_similarity_matrix_iceberg_css_fwb.npy')
-# jaccard_sim_matrix = np.load('jaccard_sim_matrix.npy')
+cosine_sim_matrix = np.load('cosine_sim_matrix_BERT_iceberg_css_hfa.npy')
 
 plt.figure(figsize=(10, 8))
 sns.heatmap(cosine_sim_matrix, annot=False, cmap='coolwarm', square=True)
-plt.title('Cosine Similarity Heatmap for Iceberg CSS Responses of FWB Case (BERT)')
-plt.savefig('cos_sim_BERT_iceberg_css_fwb.png')
+plt.title('Cosine Similarity Heatmap for Iceberg & CSS Responses of HfA Case (BERT)')
+plt.savefig('cos_sim_BERT_iceberg_css_hfa.png')
 plt.show()

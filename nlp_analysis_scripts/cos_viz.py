b52c3118c24af643e7c4af010aef9780c8d7b559
Susannah Su <susannahsu1201@gmail.com>
2024-03-30 20:52:37 -0400

    Finish updating the heatmap with uniform temperature meters for each group (BERT: 0.94 to 1, normal: 0.3 to 1)

--- nlp_analysis_scripts/cos_viz.py
+++ nlp_analysis_scripts/cos_viz.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 
 # Loading the similarity matrices
 # cosine_sim_matrix = np.load('bert_cosine_similarity_matrix.npy')
-cosine_sim_matrix = np.load('cosine_sim_matrix_BERT_iceberg_css_hfa.npy')
+cosine_sim_matrix = np.load('cosine_sim_matrix_IoA_hfa.npy')
 
 plt.figure(figsize=(10, 8))
-sns.heatmap(cosine_sim_matrix, annot=False, cmap='coolwarm', square=True)
-plt.title('Cosine Similarity Heatmap for Iceberg & CSS Responses of HfA Case (BERT)')
-plt.savefig('cos_sim_BERT_iceberg_css_hfa.png')
+sns.heatmap(cosine_sim_matrix, annot=False, cmap='coolwarm', square=True, vmin=0.3, vmax=1)
+plt.title('Cosine Similarity Heatmap for IoA of HfA Case')
+plt.savefig('cos_sim_IoA_hfa.png')
 plt.show()

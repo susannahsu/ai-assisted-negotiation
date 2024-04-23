4dedc867761bc45041a4485db5c0d369ee4b85d0
Susannah Su <susannahsu1201@gmail.com>
2024-04-23 11:56:52 -0400

    Generate new heatmaps with 0.6 as low bound

--- nlp_analysis_scripts/cos_viz.py
+++ nlp_analysis_scripts/cos_viz.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 cosine_sim_matrix = np.load('cosine_sim_matrix_IoA_hfa.npy')
 
 plt.figure(figsize=(10, 8))
-sns.heatmap(cosine_sim_matrix, annot=False, cmap='coolwarm', square=True, vmin=0.3, vmax=1)
+sns.heatmap(cosine_sim_matrix, annot=False, cmap='coolwarm', square=True, vmin=0.6, vmax=1)
 plt.title('Cosine Similarity Heatmap for IoA of HfA Case')
-plt.savefig('cos_sim_IoA_hfa.png')
+plt.savefig('cos_sim_IoA_hfa_new.png')
 plt.show()
